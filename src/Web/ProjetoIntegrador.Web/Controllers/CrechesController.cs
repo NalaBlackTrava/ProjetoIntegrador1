0b6aba6de26359ccd03eda52538253e1d744a1bb
Daniel Hideki <danielm2512@hotmail.com>
2024-05-08 00:40:53 -0300

    adding controller

---
+++ src/Web/ProjetoIntegrador.Web/Controllers/CrechesController.cs
@@ -0,0 +1,68 @@
+﻿// CrechesController.cs
+using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using ProjetoIntegrador.Application.Commands.Creches;
+using System.Threading.Tasks;
+using ProjetoIntegrador.Application.Commands.Creches.Address;
+
+namespace ProjetoIntegrador.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CrechesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CrechesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateCrecheCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return CreatedAtAction(nameof(Create), new { id = result.Id }, result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, UpdateCrecheCommand command)
+        {
+            if (id != command.Id)
+            {
+                return BadRequest();
+            }
+
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Read(int id)
+        {
+            var result = await _mediator.Send(new ReadCrecheCommand { Id = id });
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _mediator.Send(new DeleteCrecheCommand { Id = id });
+            return Ok(result);
+        }
+
+        [HttpPost("address")]
+        public async Task<IActionResult> Add(AddAddressCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok();
+        }
+
+        [HttpDelete("{crecheId}/address/{addressId}")]
+        public async Task<IActionResult> Remove(long crecheId, long addressId)
+        {
+            var result = await _mediator.Send(new RemoveAddressCommand { AddressId = addressId, CrecheId = crecheId });
+            return Ok(result);
+        }
+    }
+}
---
+++ src/Web/ProjetoIntegrador.Web/Controllers/UsersController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using ProjetoIntegrador.Application.Commands.User;
+using ProjetoIntegrador.Application.Commands.User.Address;
+
+namespace ProjetoIntegrador.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UsersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateUserCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return CreatedAtAction(nameof(Create), new { id = result.Id }, result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, UpdateUserCommand command)
+        {
+            if (id != command.Id)
+            {
+                return BadRequest();
+            }
+
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Read(int id)
+        {
+            var result = await _mediator.Send(new ReadUserCommand { Id = id });
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _mediator.Send(new DeleteUserCommand { Id = id });
+            return Ok(result);
+        }
+
+        [HttpPost("address")]
+        public async Task<IActionResult> Add(AddAddressCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok();
+        }
+
+        [HttpDelete("{userId}/address/{addressId}")]
+        public async Task<IActionResult> Remove(long userId, long addressId)
+        {
+            var result = await _mediator.Send(new RemoveAddressCommand { AddressId = addressId, UserId = userId });
+            return Ok(result);
+        }
+    }
+}
